a5a1c0ceb2782e796fefcf090d7b8342fb44b3b6
Suhan G Paradkar <76830230+suhan-paradkar@users.noreply.github.com>
2021-05-24 12:46:41 +0530

    Update update-desktop.sh

--- update-desktop.sh
+++ update-desktop.sh
@@ -8,46 +8,22 @@ sleep 5
 
 #install new packages
 
-pkg install -y ristretto
-
-pkg install -y tumbler
-
-pkg install -y mpv
-
-pkg install -y python-tkinter 
-
-pkg install -y geany-plugins
-
-pkg install -y xfce4-taskmanager
-
-pkg install -y xfce4-whiskermenu-plugin
-
-pkg install -y xfce4-clipman-plugin
+pkg up
+git pull
 
 #configuring update
 
 rm -rf $PREFIX/bin/music
-
 rm -rf $PREFIX/startdesktop 
-
 cp -rf $HOME/termux-desktop-xfce/music $PREFIX/bin
-
 cp -rf $HOME/termux-desktop-xfce/startdesktop $PREFIX/bin
-
 rm -rf $HOME/.config.old 
-
 cp -rf $HOME/.config $HOME/.config.old 
-
 rm -rf $HOME/.config 
-
 cp -rf $HOME/termux-desktop-xfce/.config $HOME
-
 cp -rf $HOME/termux-desktop-xfce/README.desktop $HOME/Desktop
-
 cp -rf $HOME/termux-desktop-xfce/.icons/* $HOME/.icons/
-
 cp -rf $HOME/termux-desktop-xfce/.themes/* $HOME/.themes/
-
 cp -rf $HOME/termux-desktop-xfce/backgrounds/* $HOME/backgrounds/
 
 clear 
