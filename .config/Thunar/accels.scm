ed38c89a7120bdfc000ee953397041a77ec82bf7
Yisus7u7 <jesuspixel5@gmail.com>
2021-06-19 21:20:03 -0500

    v3.0.9 : Nordic update

--- .config/Thunar/accels.scm
+++ .config/Thunar/accels.scm
@@ -1,4 +1,4 @@
-; Thunar GtkAccelMap rc-file         -*- scheme -*-
+; thunar GtkAccelMap rc-file         -*- scheme -*-
 ; this file is an automated accelerator map dump
 ;
 ; (gtk_accel_path "<Actions>/ThunarLauncher/delete-3" "<Shift>KP_Delete")
