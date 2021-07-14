2f673b2dc1c345902a2151cd326b93a3eaba8d44
Suhan G Paradkar <76830230+suhan-paradkar@users.noreply.github.com>
2021-07-14 20:21:48 +0530

    Update compile-install.sh
    Intltool is now added to unstable-repo.. so that will be installed from there...

--- compile-install.sh
+++ compile-install.sh
@@ -1,18 +1,6 @@
 #Optional tools that will be compiled on-device
 
-pkg install perl python libexpat
-
-cpan install XML::Parser
-cpan install XML::LibXML
-
-wget https://launchpad.net/intltool/trunk/0.51.0/+download/intltool-0.51.0.tar.gz
-tar -xvf intltool-0.51.0.tar.gz
-cd intltool-0.51.0/
-autoreconf -fi
-./configure --prefix=$PREFIX
-make
-make install
-cd ..
+pkg install unstable-repo intltool
 
 wget https://archive.xfce.org/src/xfce/xfce4-dev-tools/4.16/xfce4-dev-tools-4.16.0.tar.bz2
 tar -xvf xfce4-dev-tools-4.16.0.tar.bz2
