44c7e5aa6ee83ddd5a384cd808c10ae61b1fd222
Yisus7u7 <jesuspixel5@gmail.com>
2021-08-07 21:41:22 -0500

    fix files

--- .themes/install-flat-remix-gtk.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# To Avoid Large files storing onto my repository, this should be installed in a script
-
-TEMP=$(mktemp -d)
-
-git clone https://github.com/daniruiz/flat-remix-gtk $TEMP
-
-mv $TEMP/Flat-Remix-GTK-Blue* ~/.themes
-rm -rf $TEMP
