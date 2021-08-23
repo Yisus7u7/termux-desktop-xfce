9d25fa299c3057704351a359b460f4ba5aac09ee
Yisus7u7 <64093255+Yisus7u7@users.noreply.github.com>
2021-08-23 09:57:34 -0500

    Fix black screen (#26)

--- postins.sh
+++ postins.sh
@@ -11,4 +11,9 @@ rm breeze-cursor-theme_5.20.5-4_all.deb
 wget https://github.com/Yisus7u7/termux-desktop-xfce/releases/download/arc/termux-arc-gtk-theme_20210412_all.deb
 apt install ./termux-arc-gtk-theme_20210412_all.deb
 rm termux-arc-gtk-theme_20210412_all.deb
+rm ~/.vnc/*.pid
+rm ~/.vnc/*.log
+rm ~/.vnc/passwd 
+chmod +x ~/.vnc/xstartup 
+
 
