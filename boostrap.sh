65939547983def4168f6d9248a4c81483769f88b
c4non_dev <83752061+st0rmc4non@users.noreply.github.com>
2022-04-13 18:37:31 +0800

    no wallpapers? no problem
    :trollface: Added the wget thing from the LXQT which downloads a bunch of wallpapers

--- boostrap.sh
+++ boostrap.sh
@@ -39,6 +39,10 @@ wget https://github.com/Yisus7u7/termux-desktop-xfce/releases/download/desktop-5
 tar -xvf data.tar.xz
 rm data.tar.xz
 
+echo "Downloading wallpapers..."
+wget https://github.com/Yisus7u7/termux-desktop-lxqt/releases/download/data/termux_desktop_lxqt_data.tar.xz
+tar -xvf termux_desktop_lxqt_data.tar.xz')
+
 echo "setup folders..."
 
 mkdir $HOME/Desktop 
@@ -53,9 +57,9 @@ termux-setup-storage
 ln -s $HOME/storage/music Music 
 
 cd $HOME/Desktop
-rm LEAME.txt README.txt
 wget https://raw.githubusercontent.com/Yisus7u7/termux-desktop-xfce/main/welcome/LEAME.txt
 wget https://raw.githubusercontent.com/Yisus7u7/termux-desktop-xfce/main/welcome/README.txt
+rm LEAME.txt README.txt
 cd $HOME
 
 mv $PREFIX/share/kvantum/* $PREFIX/share/Kvantum
